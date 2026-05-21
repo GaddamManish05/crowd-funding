f80ba104a71a2f7e98f6123485f2a72ab8b9d4a0
GaddamManish05 <gaddam.mani1305@gmail.com>
2026-05-21 11:56:47 +0530

    Updated Email transporter

--- server/config/Nodemailer.js
+++ server/config/Nodemailer.js
@@ -6,10 +6,18 @@ import nodemailer from "nodemailer";
 // ==========================================
 // 2. CONFIGURATION & CONSTANTS
 // ==========================================
-export const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
+
     service: "gmail",
+
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
-    }
+    },
+
+    pool: true,
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
