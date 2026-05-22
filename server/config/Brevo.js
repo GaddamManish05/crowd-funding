495d035423edc59569e4d93bfe2eb1682645487c
GaddamManish05 <gaddam.mani1305@gmail.com>
2026-05-22 13:03:07 +0530

    fixed brevo import

--- server/config/Brevo.js
+++ server/config/Brevo.js
@@ -1,9 +1,9 @@
-import Brevo from "@getbrevo/brevo";
+import * as Brevo from "@getbrevo/brevo";
 
 const apiInstance = new Brevo.TransactionalEmailsApi();
 
 const apiKey = apiInstance.authentications["apiKey"];
 
-apiKey.apiKey = process.env.MAIL_PASS;
+apiKey.apiKey = process.env.BREVO_API_KEY;
 
 export default apiInstance;
